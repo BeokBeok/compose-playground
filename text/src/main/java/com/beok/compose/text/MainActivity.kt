68453d6e4f2caa069a16c678a32006c6064fc7fc
강현석 <kekemusa37@gmail.com>
2023-09-19 20:36:03 +0900

    [개선] 텍스트 입력 -> 커서 이동 -> 텍스트 입력 시 마지막 글자가 수정되는 현상

--- text/src/main/java/com/beok/compose/text/MainActivity.kt
+++ text/src/main/java/com/beok/compose/text/MainActivity.kt
@@ -6,25 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -32,12 +24,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.beok.compose.text.ui.theme.ComposePlaygroundTheme
 
@@ -65,6 +54,7 @@ class MainActivity : ComponentActivity() {
                         onValueChange = {
                             value = it
                         },
+                        interactionSource = interactionSource,
                         decorationBox = { innerTextField ->
                             TextFieldDefaults.DecorationBox(
                                 value = value.text,
@@ -77,6 +67,9 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                     )
+                    if (interactionSource.collectIsPressedAsState().value) {
+                        value = TextFieldValue(text = value.text, selection = value.selection)
+                    }
                 }
             }
         }
